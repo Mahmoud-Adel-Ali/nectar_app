ea75d9792bfa5931d9096bd77ecc96bd05d93b5c
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-13 18:39:54 -0700

    create onbording  view

---
+++ lib/features/onbording/presentation/view/onbording_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnbordingView extends StatelessWidget {
+  const OnbordingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Onboarding'),
+      ),
+    );
+  }
+}
