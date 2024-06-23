33992c5264952dd86e2640b7520c87941e1d9071
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-22 23:20:00 -0700

    handle details image in details view

--- lib/features/home/presentation/widgets/details_image.dart
+++ lib/features/home/presentation/widgets/details_image.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:nectar_app/core/app/app_images.dart';
@@ -6,8 +5,9 @@ import 'package:nectar_app/core/app/app_images.dart';
 class DetailsImage extends StatelessWidget {
   const DetailsImage({
     super.key,
+    required this.imgPath,
   });
-
+  final String imgPath;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,8 +31,7 @@ class DetailsImage extends StatelessWidget {
               ],
             ),
           ),
-          Image.network(
-              'https://s3-alpha-sig.figma.com/img/8590/b6a4/4c4e9d58d8f6ba361a423878cbabe793?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P6A2oMxoKXZgGxaIQNUqMnCTBs56xzpd7RzlE5zfs0LyOHPDJHhjLKoqBPCdyfn~L0IhUwajvQhqpmqHaIMzKL~3GnmQRNogqBFA-4vTaeRS8LQGFbEnH1fCCFHi0UTCykXnsaiBjN2cMMrgo3ISEYyadCH7EgXE9MpN~PQz-WvAIUUeu9l-FAjwbLdoZcHUjFVa~hJWBehP5Zb5mLxBPZnHa7Snmc4dc40zr27X8QZyRWspNSMbcE6ZTQ-9BxhjxVS6CaPSNR0FLSnMjm-SgNOnfiG~X9wWKAcvWKDl9ZYIl8SMPV6Zk033q6Q1q5goKjR4nF-MjhjBHP0VGvqfkw__'),
+          Image.network(imgPath),
         ],
       ),
     );
--- lib/features/home/presentation/widgets/details_view_body.dart
+++ lib/features/home/presentation/widgets/details_view_body.dart
@@ -8,7 +8,7 @@ class DetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: <Widget>[
-        DetailsImage(),
+        DetailsImage(imgPath: 'https://s3-alpha-sig.figma.com/img/8590/b6a4/4c4e9d58d8f6ba361a423878cbabe793?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P6A2oMxoKXZgGxaIQNUqMnCTBs56xzpd7RzlE5zfs0LyOHPDJHhjLKoqBPCdyfn~L0IhUwajvQhqpmqHaIMzKL~3GnmQRNogqBFA-4vTaeRS8LQGFbEnH1fCCFHi0UTCykXnsaiBjN2cMMrgo3ISEYyadCH7EgXE9MpN~PQz-WvAIUUeu9l-FAjwbLdoZcHUjFVa~hJWBehP5Zb5mLxBPZnHa7Snmc4dc40zr27X8QZyRWspNSMbcE6ZTQ-9BxhjxVS6CaPSNR0FLSnMjm-SgNOnfiG~X9wWKAcvWKDl9ZYIl8SMPV6Zk033q6Q1q5goKjR4nF-MjhjBHP0VGvqfkw__',),
       ],
     );
   }
