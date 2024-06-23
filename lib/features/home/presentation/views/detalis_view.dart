a97f982443c9d9353ba2adbe45babbd227e0b64d
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-22 22:37:00 -0700

    create details view

---
+++ lib/features/home/presentation/views/detalis_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetalisView extends StatelessWidget {
+  const DetalisView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/features/home/presentation/widgets/custom_product_card.dart
+++ lib/features/home/presentation/widgets/custom_product_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:nectar_app/core/common/widgets/custom_buttom.dart';
 import 'package:nectar_app/core/utils/styless.dart';
 import 'package:nectar_app/features/home/data/models/product_model.dart';
+import 'package:nectar_app/features/home/presentation/views/detalis_view.dart';
 
 class CustomProductCard extends StatelessWidget {
   const CustomProductCard({
@@ -12,55 +13,62 @@ class CustomProductCard extends StatelessWidget {
   final ProductModel productModel;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-      elevation: 1,
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Image.network(
-              productModel.imgPath,
-              height: 80,
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              title: Text(
-                productModel.name,
-                style: Styless.textStyle16Bold,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const DetalisView(),
+        ));
+      },
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        elevation: 1,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image.network(
+                productModel.imgPath,
+                height: 80,
               ),
-              subtitle: Opacity(
-                  opacity: 0.5,
-                  child: Text(
-                    productModel.description,
-                    style: Styless.textStyle14,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  )),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "\$ ${productModel.price}",
-                  style: Styless.textStyle18,
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text(
+                  productModel.name,
+                  style: Styless.textStyle16Bold,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                SizedBox(
-                  width: 55,
-                  child: CustomButtom(
-                    onPressed: () {},
-                    child: const Icon(
-                      Icons.add,
-                    ),
+                subtitle: Opacity(
+                    opacity: 0.5,
+                    child: Text(
+                      productModel.description,
+                      style: Styless.textStyle14,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    )),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "\$ ${productModel.price}",
+                    style: Styless.textStyle18,
                   ),
-                )
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 55,
+                    child: CustomButtom(
+                      onPressed: () {},
+                      child: const Icon(
+                        Icons.add,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
