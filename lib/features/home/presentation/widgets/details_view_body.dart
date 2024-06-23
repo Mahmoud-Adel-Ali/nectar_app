b5e251c34f9cc5334b4b83c8d70e39639eaa9284
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-23 09:36:24 -0700

    add product name to details view

--- lib/features/home/presentation/widgets/details_view_body.dart
+++ lib/features/home/presentation/widgets/details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nectar_app/features/home/presentation/widgets/details_image.dart';
+import 'package:nectar_app/features/home/presentation/widgets/product_name.dart';
 
 class DetailsViewBody extends StatelessWidget {
   const DetailsViewBody({super.key});
@@ -8,8 +9,13 @@ class DetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: <Widget>[
-        DetailsImage(imgPath: 'https://s3-alpha-sig.figma.com/img/8590/b6a4/4c4e9d58d8f6ba361a423878cbabe793?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P6A2oMxoKXZgGxaIQNUqMnCTBs56xzpd7RzlE5zfs0LyOHPDJHhjLKoqBPCdyfn~L0IhUwajvQhqpmqHaIMzKL~3GnmQRNogqBFA-4vTaeRS8LQGFbEnH1fCCFHi0UTCykXnsaiBjN2cMMrgo3ISEYyadCH7EgXE9MpN~PQz-WvAIUUeu9l-FAjwbLdoZcHUjFVa~hJWBehP5Zb5mLxBPZnHa7Snmc4dc40zr27X8QZyRWspNSMbcE6ZTQ-9BxhjxVS6CaPSNR0FLSnMjm-SgNOnfiG~X9wWKAcvWKDl9ZYIl8SMPV6Zk033q6Q1q5goKjR4nF-MjhjBHP0VGvqfkw__',),
+        DetailsImage(
+          imgPath:
+              'https://s3-alpha-sig.figma.com/img/3834/f4b9/c7c2628935f610ab8527d0b21e102632?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gQOQbg0KD1XAmM9jif8fYP1J7dTjC6qJNxD~c3IdfTfYdFF61bIuCKO7XIKK6Lu8NwtMWoYp5ow~m4jCZykevhPHQGDPrbWfSdq~g1lD3M-GFkVx~Ef5hhoMrATQxVNv5dkkYauoebJkwFf~ycNEGGapyS9VU2h42U8woPIx0rYUkf8HPmr9cqq4U1-iDszZGvfhjD~fMRrtLEFD95LGyGK0fLSO-ipfVDHqqINMAR0o61KJTWCL4cds8-8CRaj1wDbPo3Z6vU3gfWxI6Nc4NrHXRcmUQL483ML54cCGBtTepnWfAH4J8S5IdLvaGzBJscr7ZpO1~xtuZLIij18Nfg__',
+        ),
+        ProductName(title: 'Naturel Red Apple', subtitle: '1kg, Price')
       ],
     );
   }
 }
+
---
+++ lib/features/home/presentation/widgets/product_name.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:nectar_app/core/utils/styless.dart';
+
+class ProductName extends StatelessWidget {
+  const ProductName({super.key, required this.title, required this.subtitle});
+  final String title;
+  final String subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        title,
+        style: Styless.textStyle24Bold,
+      ),
+      subtitle: Text(
+        subtitle,
+        style: Styless.textStyle16,
+      ),
+      trailing: IconButton(
+          onPressed: () {}, icon: const Icon(Icons.favorite_outline_rounded)),
+    );
+  }
+}
