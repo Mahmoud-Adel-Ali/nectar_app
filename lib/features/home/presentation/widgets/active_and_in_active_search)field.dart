5a73cc5905984155c9d3aaea1dd1e2e8491d4401
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-27 15:35:05 -0700

    refator custom search field

--- lib/features/home/presentation/widgets/active_and_in_active_search)field.dart
+++ lib/features/home/presentation/widgets/active_and_in_active_search)field.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable, unused_element
 
 import 'package:flutter/material.dart';
 import 'package:nectar_app/core/app/app_colors.dart';
@@ -12,23 +12,32 @@ class ActiveSearchField extends StatelessWidget {
   TextEditingController? controller;
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.sizeOf(context).height;
-    return SliverPersistentHeader(
+    // final double height = MediaQuery.sizeOf(context).height;
+    return SliverAppBar(
+      backgroundColor: AppColors.scaffoldBackground,
+      floating: false,
       pinned: true,
-      delegate: _SliverAppBarDelegate(
-        minHeight: height * 0.2,
-        maxHeight: height * 0.2,
-        child: Column(
-          children: [
-            SizedBox(height: height * 0.05),
-            Container(
-              height: 51.57,
-              margin: const EdgeInsets.symmetric(horizontal: 18),
+      toolbarHeight: 75,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        background: Container(color: AppColors.scaffoldBackground),
+        title: GestureDetector(
+          onTap: () {},
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            margin: const EdgeInsets.only(top: 12),
+            decoration: BoxDecoration(
+              color: AppColors.scaffoldBackground,
+              borderRadius: BorderRadius.circular(24),
+            ),
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 50,
+              margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 3),
               decoration: BoxDecoration(
                 color: const Color(0xffF2F3F2),
                 borderRadius: BorderRadius.circular(24),
               ),
-              alignment: Alignment.center,
               child: TextField(
                 onChanged: onChanged,
                 controller: controller,
@@ -45,7 +54,7 @@ class ActiveSearchField extends StatelessWidget {
                 ),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -59,39 +68,45 @@ class InActiveSearchField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.sizeOf(context).height;
-    return SliverPersistentHeader(
+    // final double height = MediaQuery.sizeOf(context).height;
+    return SliverAppBar(
+      backgroundColor: AppColors.scaffoldBackground,
+      floating: false,
       pinned: true,
-      delegate: _SliverAppBarDelegate(
-        minHeight: height * 0.1,
-        maxHeight: height * 0.1,
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          margin: const EdgeInsets.only(top: 12),
-          decoration: BoxDecoration(
-            color: AppColors.scaffoldBackground,
-            borderRadius: BorderRadius.circular(24),
-          ),
-          alignment: Alignment.bottomCenter,
+      toolbarHeight: 75,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        background: Container(color: AppColors.scaffoldBackground),
+        title: GestureDetector(
+          onTap: () {},
           child: Container(
-            height: 52,
-          margin: const EdgeInsets.symmetric(vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            margin: const EdgeInsets.only(top: 12),
             decoration: BoxDecoration(
-              color: const Color(0xffF2F3F2),
+              color: AppColors.scaffoldBackground,
               borderRadius: BorderRadius.circular(24),
             ),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(
-                  Icons.search,
-                  size: 30,
-                ),
-                Text(
-                  'Search Store',
-                  style: Styless.textStyle14,
-                ),
-              ],
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 50,
+              margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 3),
+              decoration: BoxDecoration(
+                color: const Color(0xffF2F3F2),
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                  Text(
+                    'Search Store',
+                    style: Styless.textStyle14,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
