6525c2472d08bdaf601b9c24ff20e82bfadb144f
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-13 17:21:57 -0700

    add sign up model and sign up cubit

--- lib/features/auth/data/models/sign_up_user/sign_up_model.dart
+++ lib/features/auth/data/models/sign_up_user/sign_up_model.dart
@@ -1,15 +1,15 @@
 import 'data.dart';
 
-class RegisterModel {
+class SignUpModel {
 	int? statusCode;
 	bool? isSuccess;
 	String? message;
 	Data? data;
 
-	RegisterModel({this.statusCode, this.isSuccess, this.message, this.data});
+	SignUpModel({this.statusCode, this.isSuccess, this.message, this.data});
 
-	factory RegisterModel.fromJson(Map<String, dynamic> json) {
-		return RegisterModel(
+	factory SignUpModel.fromJson(Map<String, dynamic> json) {
+		return SignUpModel(
 			statusCode: json['statusCode'] as int?,
 			isSuccess: json['isSuccess'] as bool?,
 			message: json['message'] as String?,
--- lib/nectar_app.dart
+++ lib/nectar_app.dart
@@ -5,8 +5,8 @@ import 'package:nectar_app/core/databases/cache/cache_helper.dart';
 import 'package:nectar_app/core/databases/cache/cache_keys.dart';
 import 'package:nectar_app/core/helper/font_family_helper.dart';
 import 'package:nectar_app/core/services/services_locator.dart';
+import 'package:nectar_app/features/auth/presentation/views/login_view.dart';
 import 'package:nectar_app/features/onbording/presentation/view/onbording_view.dart';
-import 'package:nectar_app/core/app/nectar_app_views.dart';
 
 class NectarApp extends StatelessWidget {
   const NectarApp({super.key});
@@ -29,7 +29,7 @@ class NectarApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: getit<CacheHelper>().getBoolean(CacheKeys.onbordingVisited) ?? false
-          ? const NectarAppViews() // const LoginView()
+          ? const LoginView()
           : const OnbordingView(),
     );
   }
