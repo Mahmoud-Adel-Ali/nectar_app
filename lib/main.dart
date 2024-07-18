29d1fa6878997ae0e99ea6f165e45363327adc77
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-17 21:57:59 -0700

    set device not rotated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nectar_app/core/databases/cache/cache_helper.dart';
 import 'package:nectar_app/core/services/services_locator.dart';
 import 'package:nectar_app/nectar_app.dart';
@@ -7,5 +8,11 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   setupServicesLocator();
   await getit<CacheHelper>().init();
-  runApp(const NectarApp());
+  // this step used to set device not rotated
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const NectarApp());
+  });
 }
